1e573d07019cf30e78bd12fec540ff52784d4575
Oraion13 <65845210+Oraion13@users.noreply.github.com>
2022-08-11 17:41:44 +0530

    mongo db initialized

--- app.ts
+++ app.ts
@@ -1,7 +1,17 @@
 import express, { Application, Request, Response } from "express";
+const mongoose = require("mongoose");
+const { MONGO_IP, MONGO_PORT, MONGO_USERNAME, MONGO_PASSWORD } = require("./config/config");
 
+// create a express app
 const app: Application = express();
 
+// conect to db
+const mongoURL = `mongodb://${MONGO_USERNAME}:${MONGO_PASSWORD}@${MONGO_IP}:${MONGO_PORT}/?authSource=admin`
+mongoose.connect(mongoURL).then(() => {
+    console.log("Connected to DB");
+}).catch((error: any) => {
+    console.error(error)
+})
 // to parse body (inputs)
 app.use(express.json());
 
--- config/config.ts
+++ config/config.ts
@@ -1,3 +1,9 @@
 module.exports = {
-    APP_PORT: process.env.PORT || 3000
+    APP_PORT: process.env.PORT || 3000,
+    MONGO_IP: process.env.MONGO_IP || "mongo",
+    MONGO_PORT: process.env.MONGO_PORT || 27017,
+    MONGO_USERNAME: process.env.MONGO_USERNAME,
+    MONGO_PASSWORD: process.env.MONGO_PASSWORD,
+    REDIS_IP: process.env.REDIS_IP || "redis",
+    REDIS_PORT: process.env.REDIS_PORT || 6379
 }
